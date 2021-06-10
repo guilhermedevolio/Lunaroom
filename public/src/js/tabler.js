5224a4e392b5909b5a740e637e03905ceefd3e57
Guilherme5889 <guinogaroto@gmail.com>
2021-06-10 11:01:25 -0300

    feat(template) - Add template resources

---
+++ public/src/js/tabler.js
@@ -0,0 +1,12 @@
+//Vendor
+
+import './src/autosize';
+import './src/input-mask';
+import './src/dropdown';
+import './src/tooltip';
+import './src/popover';
+import './src/switch-icon';
+import { EnableActivationTabsFromLocationHash } from './src/tab';
+import './src/toast';
+
+EnableActivationTabsFromLocationHash();
