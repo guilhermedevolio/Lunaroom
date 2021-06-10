bd07f5ad8d428e3cca4c344a08bb44a23e94f42e
Guilherme5889 <guinogaroto@gmail.com>
2021-06-10 11:19:47 -0300

    feat(Authentication) - Add Authentication Controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class AuthController extends Controller
+{
+    public function viewLogin(): View
+    {
+        return view('login');
+    }
+}
